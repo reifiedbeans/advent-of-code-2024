bb8da76d58e9a5d94aa95e5ad6df1f7b5e795b98
Drew Davis <drew@reifiedbeans.dev>
2024-11-30 23:48:16 -0800

    add caution message for toPair

--- src/main/kotlin/dev/reifiedbeans/adventofcode2024/Utils.kt
+++ src/main/kotlin/dev/reifiedbeans/adventofcode2024/Utils.kt
@@ -6,6 +6,10 @@ fun getInput(name: String) = File("input", "$name.txt")
 
 fun File.readLinesChunked(delimiter: String = "\n\n") = this.readText().trim('\n').split(delimiter).map(String::lines)
 
+/**
+ * Converts a [List] of two elements to a [Pair]. Written for convenience, but I don't condone it. Use caution.
+ * @throws IllegalArgumentException if the [List] does not have exactly two elements
+ */
 fun <T : Any> List<T>.toPair(): Pair<T, T> {
     require(this.size == 2) { "List must have only two elements" }
     return this.first() to this.last()
