7edceec697b6c51b136b079ebf18c07aae392159
Drew Davis <drew@reifiedbeans.dev>
2024-11-30 23:44:58 -0800

    add Day01

---
+++ src/main/kotlin/dev/reifiedbeans/adventofcode2024/Day01.kt
@@ -0,0 +1,24 @@
+package dev.reifiedbeans.adventofcode2024
+
+import kotlin.math.abs
+
+private fun part1(list1: List<Int>, list2: List<Int>): Int {
+    val pairs = list1.sorted() zip list2.sorted()
+    return pairs.sumOf { (first, second) -> abs(first - second) }
+}
+
+private fun part2(list1: List<Int>, list2: List<Int>) = list1.fold(0) { score, num ->
+    score + (list2.count { it == num } * num)
+}
+
+private fun List<String>.parse() = this.map { it.split("\\s+".toRegex()).map(String::toInt).toPair() }.unzip()
+
+fun main() {
+    val (testList1, testList2) = getInput("Day01_test").readLines().parse()
+    check(part1(testList1, testList2) == 11)
+    check(part2(testList1, testList2) == 31)
+
+    val (list1, list2) = getInput("Day01").readLines().parse()
+    println("Part 1: ${part1(list1, list2)}")
+    println("Part 2: ${part2(list1, list2)}")
+}
--- src/main/kotlin/dev/reifiedbeans/adventofcode2024/Utils.kt
+++ src/main/kotlin/dev/reifiedbeans/adventofcode2024/Utils.kt
@@ -5,3 +5,8 @@ import java.io.File
 fun getInput(name: String) = File("input", "$name.txt")
 
 fun File.readLinesChunked(delimiter: String = "\n\n") = this.readText().trim('\n').split(delimiter).map(String::lines)
+
+fun <T : Any> List<T>.toPair(): Pair<T, T> {
+    require(this.size == 2) { "List must have only two elements" }
+    return this.first() to this.last()
+}
