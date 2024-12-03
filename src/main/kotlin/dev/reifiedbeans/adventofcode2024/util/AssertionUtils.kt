f6bea6c01c8cf62b8dd36267e44cf95e6fe10229
Drew Davis <drew@reifiedbeans.dev>
2024-12-02 23:23:50 -0800

    remove generic param from assertEquals

--- src/main/kotlin/dev/reifiedbeans/adventofcode2024/util/AssertionUtils.kt
+++ src/main/kotlin/dev/reifiedbeans/adventofcode2024/util/AssertionUtils.kt
@@ -3,6 +3,6 @@ package dev.reifiedbeans.adventofcode2024.util
 /**
  * Simple reimplementation of JUnit's assertEquals method.
  */
-inline fun <reified T : Any> assertEquals(expected: T, actual: T) {
+fun assertEquals(expected: Any, actual: Any) {
     if (expected != actual) throw AssertionError("Expected <$expected> but was <$actual>")
 }
